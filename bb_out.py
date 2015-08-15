21b2395475ca4189b146008264b753f474342001
HarJIT <harjit@harjit.moe>
2015-08-15 14:40:02 +0100

    Refine, allow output of traditional BBCode.
    Signed-off-by: HarJIT <harjit@harjit.moe>

---
+++ bb_out.py
@@ -0,0 +1,47 @@
+from nodes import *
+
+def bb_out(nodes):
+    in_list=0
+    r=""
+    for node in nodes:
+        _r=_bb_out(node,in_list)
+        if len(_r)==2 and type(_r)==type(()):
+            _r,in_list=_r
+        r+=_r
+    return r.strip("\r\n")
+
+def _bb_out(node,in_list):
+    if in_list and not isinstance(node,UlliNode):
+        return "[/list]\n"+_bb_out(node,False),False
+    if isinstance(node,basestring):
+        return node
+    elif isinstance(node,TitleNode):
+        #Yeah, BBCode sucks at titles
+        return ("\n[size=%d][b]"%(8-node.depth))+bb_out(node.content)+"[/b][/font]\n"
+    elif isinstance(node,ParagraphNode):
+        return "\n"+bb_out(node.content)+"\n"
+    elif isinstance(node,BlockQuoteNode):
+        return "\n[quote]"+bb_out(node.content)+"[/quote]\n"
+    elif isinstance(node,CodeBlockNode):
+        return "\n[code]"+bb_out(node.content)+"[/code]\n"
+    elif isinstance(node,UlliNode):
+        r="[*]"+bb_out(node.content)+"\n"
+        if not in_list:
+            r="\n[list]"+r
+        return r,True
+    elif isinstance(node,BoldNode):
+        return "[b]"+bb_out(node.content)+"[/b]"
+    elif isinstance(node,ItalicNode):
+        return "[i]"+bb_out(node.content)+"[/i]"
+    elif isinstance(node,HrefNode):
+        if node.hreftype=="link":
+            return ("[url=%r]"%node.content)+bb_out(node.label)+"[/url]"
+        elif node.hreftype=="img":
+            return "[img]"+node.content+"[/img]"
+        else:
+            #e.g. [media]...[/media]
+            return "["+node.hreftype+"]"+node.content+"[/media]"
+    elif isinstance(node,NewlineNode):
+        return "[br]"
+    else:
+        return "ERROR"+repr(node)
--- cool_parser.py
+++ cool_parser.py
@@ -1,98 +1,14 @@
+from nodes import *
+
 try:
     from io import StringIO
 except:
     from StringIO import StringIO
 
-class Node(object):
-    pass
-
-class NonContainerNode(Node):
-    pass
-
-class BlockContainerNode(Node):
-    def __init__(self,content,depth=-1,number=None):
-        self.content=list(parse_block(content))
-        self.depth=depth
-        self.number=number
-
-class InlineContainerNode(Node):
-    def __init__(self,content,depth=-1,number=None):
-        self.content=list(parse_inline(content))
-        self.depth=depth
-        self.number=number
-
-class RawContainerNode(Node):
-    def __init__(self,content,depth=-1,number=None,clas=None):
-        self.content=content
-        self.depth=depth
-        self.number=number
-        self.clas=clas
-
-class TitleNode(InlineContainerNode):
-    pass
-
-class ParagraphNode(InlineContainerNode):
-    pass
-
-class BlockQuoteNode(BlockContainerNode):
-    pass
-
-class UlliNode(BlockContainerNode):
-    pass
-
-class OlliNode(BlockContainerNode):
-    pass
-
-class CodeBlockNode(RawContainerNode):
-    pass
-
-class RuleNode(NonContainerNode):
-    pass
-
 try:
     StopIteration=StopIteration
 except NameError:
     StopIteration=IndexError
-
-class LinestackIter(object):
-    _stack=None
-    _n=None
-    _c=None
-    def __init__(self,stack):
-        self._stack=list(stack)
-        self._n=-1
-        self._c=0 #Number of calls to __next__
-    def __next__(self):
-        self._c+=1
-        self._n+=1
-        if self._n==len(self._stack):
-            return ""
-        if self._n>=(len(self._stack)+1):
-            raise StopIteration
-        return self._stack[self._n]
-    def peek_back(self,n=1):
-        if (self._n-n)<0:
-            return None
-        return self._stack[self._n-n]
-    def peek_ahead(self,n=1):
-        if (self._n+n)>=len(self._stack):
-            return None
-        return self._stack[self._n+n]
-    def rtpma(self): #Run that past me again
-        self._n-=1
-    #
-    # Tell Python to use iterator __next__/next API
-    def __iter__(self):
-        return self
-    #
-    # Wrappers for older iterator APIs
-    def next(self):
-        return self.__next__()
-    def __getitem__(self,i):
-        if i==self._c:
-            return self.__next__()
-        else:
-            raise TypeError("indexing an iterator")
 
 def all_same(l):
     if len(l)==0:
@@ -114,7 +30,6 @@ def _parse_block(f):
     fenceinfo=None
 
     import re
-    rule_re=" ? ? ?((- ?)(- ?)(- ?)+|(_ ?)(_ ?)(_ ?)+|(\* ?)(\* ?)(\* ?)+)"
     isheadatx=lambda line:line.strip() and line.startswith("#") and (not line.lstrip("#")[:1].strip()) and ((len(line)-len(line.lstrip("#")))<=6)
     isulin=lambda line:line.strip() and ((len(line)-len(line.lstrip()))<=3) and (all_same(line.strip()) in tuple("=-"))
     isfence=lambda line:line.strip() and ((len(line)-len(line.lstrip()))<=3) and (line.lstrip()[0]==line.lstrip()[1]==line.lstrip()[2]) and (line.lstrip()[0] in "`~") and ((line.lstrip()[0]=="~") or ("`" not in line.lstrip().lstrip("`")))
@@ -135,14 +50,6 @@ def _parse_block(f):
                 continue
             elif isul(line):
                 within="ul"
-                f.rtpma()
-                continue
-            elif line.lstrip() and line.lstrip()[0] in "0123456789":
-                within="ol"
-                f.rtpma()
-                continue
-            elif re.match(rule_re, line.rstrip()):
-                within="rule"
                 f.rtpma()
                 continue
             elif isfence(line):
@@ -194,11 +101,6 @@ def _parse_block(f):
                 within="root"
                 f.rtpma()
                 continue
-            elif re.match(rule_re, line.rstrip()):
-                within="rule"
-                depth=0
-                f.rtpma()
-                continue
             if line.rstrip("\r\n").endswith("  "):
                 minibuf+=line.strip()+"\n"
             else:
@@ -210,14 +112,6 @@ def _parse_block(f):
                 depth=0
                 depths=[]
                 within="root"
-                f.rtpma()
-                continue
-            elif re.match(rule_re, line.rstrip()):
-                yield (UlliNode(minibuf,depth))
-                minibuf=""
-                depth=0
-                depths=[]
-                within="rule"
                 f.rtpma()
                 continue
             elif isul(line):
@@ -237,53 +131,6 @@ def _parse_block(f):
                 minibuf+=line.lstrip()[1:].strip()+" "
             else:
                 minibuf+=line.strip()+" "
-        elif within=="ol":
-            if not line.strip():
-                yield (OlliNode(minibuf,depth,number))
-                minibuf=""
-                depth=number=0
-                depths=[]
-                within="root"
-                f.rtpma()
-                continue
-            elif re.match(rule_re, line.rstrip()):
-                yield (OlliNode(minibuf,depth,number))
-                minibuf=""
-                depth=number=0
-                depths=[]
-                within="rule"
-                f.rtpma()
-                continue
-            elif line.lstrip()[0] in "0123456789":
-                if minibuf:
-                    yield (OlliNode(minibuf,depth,number))
-                    minibuf=""
-                deep=len(line.replace("\t"," "*4))-len(line.replace("\t"," "*4).lstrip())
-                if not depths:
-                    depths.append(deep)
-                    depth=0
-                elif deep>depths[-1]:
-                    depths.append(deep)
-                    depth+=1
-                elif deep in depths:
-                    depth=depths.index(deep)
-                    depths=depths[:depth+1]
-                number=line.lstrip().split()[0].split(".")[0].split(")")[0]
-                minibuf+=line.lstrip()[len(number):].lstrip(".)").strip()+" "
-                number=int(number,10)
-            else:
-                minibuf+=line.strip()+" "
-        elif within=="rule":
-            yield (RuleNode())
-            within="root"
-        elif within=="codeblock":
-            if (not line.replace("\t"," "*4).startswith("    ")) and line.rstrip():
-                yield (CodeBlockNode(minibuf))
-                minibuf=""
-                within="root"
-                f.rtpma()
-                continue
-            minibuf+=line[4:].rstrip("\r\n")+"\n"
         elif within=="fence":
             if fence==None:
                 fence=0
@@ -307,7 +154,7 @@ def _parse_block(f):
                         line=line[1:]
                 minibuf+=line.rstrip("\r\n")+"\n"
         elif within=="quote":
-            if isbq(line) or (line.strip() and not iscb(line) and not isulin(line) and not isfence(line) and not re.match(rule_re, line.rstrip()) and not isul(line)):
+            if isbq(line) or (line.strip() and not iscb(line) and not isulin(line) and not isfence(line) and not isul(line) and not isheadatx(line)):
                 line=line.lstrip()
                 if line[:1]==">":line=line[1:]
                 if line[:1]==" ":line=line[1:]
@@ -334,7 +181,68 @@ def _parse_block(f):
 def parse_block(content):
     return _parse_block(LinestackIter(StringIO(content)))
 
-parse_inline=lambda f:[f] #For now
+def _parse_inline(content,lev="root"):
+    lastchar=" "
+    out=[]
+    import re
+    while content:
+        c=content.pop(0)
+        if c=="\\":
+            c2=content.pop(0)
+            if c2==" ":
+                lastchar=" "
+                continue
+            else:
+                lastchar=c+c2
+                out.append(c2)
+                continue
+        elif c=="\n":
+            out.append(NewlineNode())
+        elif c=="*":
+            if content[0]=="*":
+                del content[0]
+                if lev=="bold":
+                    return out
+                out.append(BoldNode(_parse_inline(content,"bold")))
+            else:
+                if lev=="italic":
+                    return out
+                out.append(ItalicNode(_parse_inline(content,"italic")))
+        elif c=="]" and lev=="label":
+            return out
+        #re.match, i.e. looks only at start of string
+        elif lastchar==" " and re.match("(!\w*)?\[.*\]\(.*\)",c+("".join(content))):
+            hreftype=""
+            while c!="[":
+                hreftype+=c
+                c=content.pop(0)
+            label=_parse_inline(content,"label")
+            href=""
+            if content[0]=="(":
+                del content[0]
+                c=content.pop(0)
+                while c!=")":
+                    href+=c
+                    c=content.pop(0)
+            if hreftype=="!":
+                hreftype="img"
+            elif hreftype=="":
+                hreftype="link"
+            else:
+                hreftype=hreftype[1:] #Minus the leading !
+            out.append(HrefNode(href,label,hreftype))
+        else:
+            lastchar=c
+            out.append(c)
+    return out
 
-doc=list(_parse_block(LinestackIter(f)))
+def parse_inline(content):
+    return _parse_inline(list(content))
 
+#parse_inline=lambda f:[f] #For now
+
+if __name__=="__main__":
+    doc=list(_parse_block(LinestackIter(f)))
+    import bb_out
+    dd=bb_out.bb_out(doc)
+    print dd

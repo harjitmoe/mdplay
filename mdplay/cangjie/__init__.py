174eef0a142d306c3a57e2694cf86cf16fd5ffec
HarJIT <harjit@harjit.moe>
2016-07-08 14:04:36 +0100

    6.0.5 - more Sôketsu adjustments (completely disregard frequency "data" from v5 table)

--- mdplay/cangjie/__init__.py
+++ mdplay/cangjie/__init__.py
@@ -3,40 +3,46 @@ from mdplay.cangjie.tables import cang03, cang05
 class Agogo(dict):
     def __getitem__(self, key):
         if key not in self:
-            self[key]=[]
+            self[key] = []
         return dict.__getitem__(self, key)
 
 class Zagogo(dict):
     def __getitem__(self, key):
         if key not in self:
-            self[key]=0
+            self[key] = 1 #Note not 0
         return dict.__getitem__(self, key)
 
 def proc_cang(cang):
     cang = cang.strip().split("\n")
     d = Agogo()
+    dd = Zagogo()
     for can in cang:
         if can[0] in "#z":
             continue
         cangjie, kanji, priori = can.split()
         priori = int(priori, 10)
         d[cangjie].append((kanji, priori))
-    return d
+        dd[kanji] = priori
+    return d, dd
 
 cangdb = {}
-cangdb[3] = proc_cang(cang03)
-cangdb[5] = proc_cang(cang05)
+popdb = {}
+cangdb[3], popdb[3] = proc_cang(cang03)
+cangdb[5], popdb[5] = proc_cang(cang05)
 
 def proc_cang(stri, version = -1):
     if version==3:
         cdb = cangdb[3]
         ncdb = cangdb[5]
+        nntub = popdb[3]
     elif version==5:
         cdb = cangdb[5]
         ncdb = cangdb[3]
+        nntub = popdb[3]
     elif version==-1:
         cdb = cangdb[3]
         ncdb = cangdb[5]
+        nntub = popdb[3]
     else:
         raise ValueError("unsupported revision number of Cangjie or not an int: %r"%version)
     stri = stri.decode("utf-8")+u"-"
@@ -58,18 +64,16 @@ def proc_cang(stri, version = -1):
                 tub = tub[:-1]
             if tub:
                 #Recall that cdb and ncdb are Agogo instances
-                if cdb[tub] or ((version<0) and ncdb[tub]):
-                    if version!=-1:
-                        nntub = Zagogo(ncdb[tub])
-                        outps = sorted(cdb[tub], key=lambda a:("%12d%s"%(100000000000-(a[1]*nntub[a[0]]),a[0])))
-                    else:
-                        mydb = Zagogo(cdb[tub])
-                        for k,v in ncdb[tub]:
-                            if k in mydb:
-                                mydb[k] *= v
-                            else:
-                                mydb[k] = v
-                        outps = sorted(mydb.items(), key=lambda a:("%12d%s"%(100000000000-a[1],a[0])))
+                if (cdb[tub] or ncdb[tub]) and (version==-1):
+                    mydb = Zagogo(cdb[tub])
+                    for k,v in ncdb[tub]:
+                        if k not in mydb:
+                            mydb[k] = v*2 #Scale differences between the tables
+                    outps = sorted(mydb.items(), key=lambda a:("%12d%s"%(100000000000-nntub[a[0]],a[0])))
+                    ind %= len(outps)
+                    out += outps[ind][0]
+                elif cdb[tub]:
+                    outps = sorted(cdb[tub], key=lambda a:("%12d%s"%(100000000000-nntub[a[0]],a[0])))
                     ind %= len(outps)
                     out += outps[ind][0]
                 else:

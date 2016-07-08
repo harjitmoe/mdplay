78d5321dce09271dfcdc0fb4216f69c225625f55
Thomas Hori <thomas.hori@liddicott.com>
2016-07-08 13:40:00 +0100

    6.0.3 - adjustments to Sôketsu (Cangjie) support

--- README.md
+++ README.md
@@ -8,12 +8,13 @@ flags can turn features and extensions off and configure exporters, the obvious 
 
 (this readme and changelog, barring the licence, is written with lowercase at the beginning of sentences, except for proper nouns, and with double spacing after sentences (in the Markdown, at any rate).  this is a deliberate stylistic decision and, as this is my project, my decisions.)
 
-code version 6.0.2
+code version 6.0.3
 
 change-log:
 
 |ver|changes|
 |---|---|
+|6.0.3|adjustments to Sôketsu (Cangjie) support (the v5 DB prioritised punctuation but not Kanji, so use the v3 list for that - this naturally drops the priority of codes which changed.....), also, make the unversioned syntax accept both versions.)|
 |6.0.2|bugfixing, improved rubi support.|
 |6.0.1|romkan support, potentially useful for !hkana[](Nihongo) input.|
 |6.0|experimental cangjie input support, potentially useful for !cang[](a dm yrmmr) input (sorts per priority in the source Ibus database, then per codepoint).|
--- mdplay/cangjie/__init__.py
+++ mdplay/cangjie/__init__.py
@@ -6,6 +6,12 @@ class Agogo(dict):
             self[key]=[]
         return dict.__getitem__(self, key)
 
+class Zagogo(dict):
+    def __getitem__(self, key):
+        if key not in self:
+            self[key]=0
+        return dict.__getitem__(self, key)
+
 def proc_cang(cang):
     cang = cang.strip().split("\n")
     d = Agogo()
@@ -21,10 +27,18 @@ cangdb = {}
 cangdb[3] = proc_cang(cang03)
 cangdb[5] = proc_cang(cang05)
 
-def proc_cang(stri, version = 3):
-    if version not in cangdb:
+def proc_cang(stri, version = -1):
+    if version==3:
+        cdb = cangdb[3]
+        ncdb = cangdb[5]
+    elif version==5:
+        cdb = cangdb[5]
+        ncdb = cangdb[3]
+    elif version==-1:
+        cdb = cangdb[3]
+        ncdb = cangdb[5]
+    else:
         raise ValueError("unsupported revision number of Cangjie or not an int: %r"%version)
-    cdb = cangdb[version]
     stri = stri.decode("utf-8")+u"-"
     tub = u""
     out = u""
@@ -43,11 +57,22 @@ def proc_cang(stri, version = 3):
                 ind += 1
                 tub = tub[:-1]
             if tub:
-                if tub not in cdb:
-                    out += "<"+tub+">"
-                else:
-                    outps = sorted(cdb[tub], key=lambda a:("%10d%s"%(100000000-a[1],a[0])))
+                #Recall that cdb and ncdb are Agogo instances
+                if cdb[tub] or ((version<0) and ncdb[tub]):
+                    if version!=-1:
+                        nntub = Zagogo(ncdb[tub])
+                        outps = sorted(cdb[tub], key=lambda a:("%12d%s"%(100000000000-(a[1]*nntub[a[0]]),a[0])))
+                    else:
+                        mydb = Zagogo(cdb[tub])
+                        for k,v in ncdb[tub]:
+                            if k in mydb:
+                                mydb[k] *= v
+                            else:
+                                mydb[k] = v
+                        outps = sorted(mydb.items(), key=lambda a:("%12d%s"%(100000000000-a[1],a[0])))
                     ind %= len(outps)
                     out += outps[ind][0]
+                else:
+                    out += "<"+tub+">"
             tub = u""
     return out

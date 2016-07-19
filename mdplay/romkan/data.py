9492f42c1c3479a3f07a3ea3da47bc80d0795fda
HarJIT <harjit@harjit.moe>
2016-07-19 21:25:44 +0100

    6.0.8 - romkan improvements including precalculation (as it were)

--- mdplay/romkan/data.py
+++ mdplay/romkan/data.py
@@ -46,85 +46,122 @@ except ImportError:
 # subsequent modifications, see romkan/LICENSE file.
 #
 
-from .ptables import *
+import os, pickle
 
-# Use Katakana
+try:
+    locals().update(pickle.loads(open(os.path.join(os.path.dirname(__file__),"data.pickle"),"rb").read()))
+except (EnvironmentError,EOFError):
+    from .ptables import *
 
-KANROM = {}
-KANROM_HBN = {}
-KANROM_KNR = {}
-ROMKAN = {}
-ROMKAN_HBN = {}
-ROMKAN_KNR = {}
+    # Use Katakana
 
-for kana, roma in HEPBURNTAB+KUNREITAB:
-    KANROM[kana] = roma
-    ROMKAN[roma] = kana
-for kana, roma in HEPBURNTAB:
-    KANROM_HBN[kana] = roma
-    ROMKAN_HBN[roma] = kana
-for kana, roma in KUNREITAB:
-    KANROM_KNR[kana] = roma
-    ROMKAN_KNR[roma] = kana
-KANROM['ン']="n'"
-KANROM_HBN['ン']="n'"
-KANROM_KNR['ン']="n'"
+    KANROM = {}
+    KANROM_HBN = {}
+    KANROM_KNR = {}
+    ROMKAN = {}
+    ROMKAN_HBN = {}
+    ROMKAN_KNR = {}
 
-# Sort in long order so that a longer Romaji sequence precedes.
+    for kana, roma in HEPBURNTAB+KUNREITAB:
+        KANROM[kana] = roma
+        ROMKAN[roma] = kana
+    for kana, roma in HEPBURNTAB:
+        KANROM_HBN[kana] = roma
+        ROMKAN_HBN[roma] = kana
+    for kana, roma in KUNREITAB:
+        KANROM_KNR[kana] = roma
+        ROMKAN_KNR[roma] = kana
+    KANROM['ン']="n'"
+    KANROM_HBN['ン']="n'"
+    KANROM_KNR['ン']="n'"
 
-_len_cmp = lambda x: -len(x)
-ROMPAT = re.compile("|".join(sorted(ROMKAN.keys(), key=_len_cmp)) )
-ROMPAT_HBN = re.compile("|".join(sorted(ROMKAN_HBN.keys(), key=_len_cmp)) )
-ROMPAT_KNR = re.compile("|".join(sorted(ROMKAN_KNR.keys(), key=_len_cmp)) )
+    # Sort in long order so that a longer Romaji sequence precedes.
 
-def _kanpat_cmp(KANROM):
-    return lambda x, y: (len(y) > len(x)) - (len(y) < len(x)) or (len(KANROM[x]) > len(KANROM[x])) - (len(KANROM[x]) < len(KANROM[x]))
-KANPAT = re.compile("|".join(sorted(KANROM.keys(), key=cmp_to_key(_kanpat_cmp(KANROM)))))
-KANPAT_HBN = re.compile("|".join(sorted(KANROM_HBN.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_HBN)))))
-KANPAT_KNR = re.compile("|".join(sorted(KANROM_KNR.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_KNR)))))
+    def _len_cmp(x): return -len(x)
+    RROMPAT = ("|".join(sorted(ROMKAN.keys(), key=_len_cmp)).replace(".","\\.") )
+    RROMPAT_HBN = ("|".join(sorted(ROMKAN_HBN.keys(), key=_len_cmp)).replace(".","\\.") )
+    RROMPAT_KNR = ("|".join(sorted(ROMKAN_KNR.keys(), key=_len_cmp)).replace(".","\\.") )
 
-KUNREI_SEQS = [y for (x, y) in KUNREITAB]
-HEPBURN_SEQS = [y for (x, y) in HEPBURNTAB]
+    def _kanpat_cmp(KANROM):
+        return lambda x, y: (len(y) > len(x)) - (len(y) < len(x)) or (len(KANROM[x]) > len(KANROM[x])) - (len(KANROM[x]) < len(KANROM[x]))
+    KKANPAT = ("|".join(sorted(KANROM.keys(), key=cmp_to_key(_kanpat_cmp(KANROM)))))
+    KKANPAT_HBN = ("|".join(sorted(KANROM_HBN.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_HBN)))))
+    KKANPAT_KNR = ("|".join(sorted(KANROM_KNR.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_KNR)))))
 
-KUNPAT = re.compile("|".join(sorted(KUNREI_SEQS, key=_len_cmp)) )
-HEPPAT = re.compile("|".join(sorted(HEPBURN_SEQS, key=_len_cmp)) )
+    KUNREI_SEQS = [y for (x, y) in KUNREITAB]
+    HEPBURN_SEQS = [y for (x, y) in HEPBURNTAB]
+
+    KKUNPAT = ("|".join(sorted(KUNREI_SEQS, key=_len_cmp)).replace(".","\\.") )
+    HHEPPAT = ("|".join(sorted(HEPBURN_SEQS, key=_len_cmp)).replace(".","\\.") )
 
 
-# Use Hiragana
+    # Use Hiragana
 
-KANROM_H = {}
-ROMKAN_H = {}
-KANROM_H_HBN = {}
-ROMKAN_H_HBN = {}
-KANROM_H_KNR = {}
-ROMKAN_H_KNR = {}
+    KANROM_H = {}
+    ROMKAN_H = {}
+    KANROM_H_HBN = {}
+    ROMKAN_H_HBN = {}
+    KANROM_H_KNR = {}
+    ROMKAN_H_KNR = {}
 
-for pair in HEPBURNTAB_H+KUNREITAB_H:
-    kana, roma = pair
-    KANROM_H[kana] = roma
-    ROMKAN_H[roma] = kana
-for kana, roma in HEPBURNTAB_H:
-    KANROM_H_HBN[kana] = roma
-    ROMKAN_H_HBN[roma] = kana
-for kana, roma in KUNREITAB_H:
-    KANROM_H_KNR[kana] = roma
-    ROMKAN_H_KNR[roma] = kana
-KANROM_H['ん']="n'"
-KANROM_H_HBN['ん']="n'"
-KANROM_H_KNR['ん']="n'"
+    for pair in HEPBURNTAB_H+KUNREITAB_H:
+        kana, roma = pair
+        KANROM_H[kana] = roma
+        ROMKAN_H[roma] = kana
+    for kana, roma in HEPBURNTAB_H:
+        KANROM_H_HBN[kana] = roma
+        ROMKAN_H_HBN[roma] = kana
+    for kana, roma in KUNREITAB_H:
+        KANROM_H_KNR[kana] = roma
+        ROMKAN_H_KNR[roma] = kana
+    KANROM_H['ん']="n'"
+    KANROM_H_HBN['ん']="n'"
+    KANROM_H_KNR['ん']="n'"
 
-# Sort in long order so that a longer Romaji sequence precedes.
+    # Sort in long order so that a longer Romaji sequence precedes.
 
-ROMPAT_H = re.compile("|".join(sorted(ROMKAN_H.keys(), key=_len_cmp)) )
-ROMPAT_H_HBN = re.compile("|".join(sorted(ROMKAN_H_HBN.keys(), key=_len_cmp)) )
-ROMPAT_H_KNR = re.compile("|".join(sorted(ROMKAN_H_KNR.keys(), key=_len_cmp)) )
+    RROMPAT_H = ("|".join(sorted(ROMKAN_H.keys(), key=_len_cmp)).replace(".","\\.") )
+    RROMPAT_H_HBN = ("|".join(sorted(ROMKAN_H_HBN.keys(), key=_len_cmp)).replace(".","\\.") )
+    RROMPAT_H_KNR = ("|".join(sorted(ROMKAN_H_KNR.keys(), key=_len_cmp)).replace(".","\\.") )
 
-KANPAT_H = re.compile("|".join(sorted(KANROM_H.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H)))))
-KANPAT_H_HBN = re.compile("|".join(sorted(KANROM_H_HBN.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H_HBN)))))
-KANPAT_H_KNR = re.compile("|".join(sorted(KANROM_H_KNR.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H_KNR)))))
+    KKANPAT_H = ("|".join(sorted(KANROM_H.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H)))))
+    KKANPAT_H_HBN = ("|".join(sorted(KANROM_H_HBN.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H_HBN)))))
+    KKANPAT_H_KNR = ("|".join(sorted(KANROM_H_KNR.keys(), key=cmp_to_key(_kanpat_cmp(KANROM_H_KNR)))))
 
-KUNREI_H = [y for (x, y) in KUNREITAB_H]
-HEPBURN_H = [y for (x, y) in HEPBURNTAB_H]
+    KUNREI_H = [y for (x, y) in KUNREITAB_H]
+    HEPBURN_H = [y for (x, y) in HEPBURNTAB_H]
 
-KUNPAT_H = re.compile("|".join(sorted(KUNREI_H, key=_len_cmp)) )
-HEPPAT_H = re.compile("|".join(sorted(HEPBURN_H, key=_len_cmp)) )
+    KKUNPAT_H = ("|".join(sorted(KUNREI_H, key=_len_cmp)).replace(".","\\.") )
+    HHEPPAT_H = ("|".join(sorted(HEPBURN_H, key=_len_cmp)).replace(".","\\.") )
+    _fn = os.path.join(os.path.dirname(__file__),"data.pickle")
+    def _dd(a, p=pickle): return p.dumps(a,2)
+    del unicodedata
+    del os
+    del pickle
+    dob = {}
+    for i in locals().keys():
+        if i[0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            dob[i] = locals()[i]
+    open(_fn,"wb").write(_dd(dob))
+
+ROMPAT = re.compile(RROMPAT)
+ROMPAT_HBN = re.compile(RROMPAT_HBN)
+ROMPAT_KNR = re.compile(RROMPAT_KNR)
+
+KANPAT = re.compile(KKANPAT)
+KANPAT_HBN = re.compile(KKANPAT_HBN)
+KANPAT_KNR = re.compile(KKANPAT_KNR)
+
+KUNPAT = re.compile(KKUNPAT)
+HEPPAT = re.compile(HHEPPAT)
+
+ROMPAT_H = re.compile(RROMPAT_H)
+ROMPAT_H_HBN = re.compile(RROMPAT_H_HBN)
+ROMPAT_H_KNR = re.compile(RROMPAT_H_KNR)
+
+KANPAT_H = re.compile(KKANPAT_H)
+KANPAT_H_HBN = re.compile(KKANPAT_H_HBN)
+KANPAT_H_KNR = re.compile(KKANPAT_H_KNR)
+
+KUNPAT_H = re.compile(KKUNPAT_H)
+HEPPAT_H = re.compile(HHEPPAT_H)

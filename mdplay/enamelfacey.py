d1c22e80e78153d5dde92bfd708a4aabb1e1073d
Thomas Hori <thomas.hori@liddicott.com>
2016-08-03 16:44:54 +0100

    minor adjustment to variable naming

--- mdplay/enamelfacey.py
+++ mdplay/enamelfacey.py
@@ -17,16 +17,16 @@ def _write_data(writer, data):
         data = simul_replace(data, "[", "[lbrack]", "]", "[rbrack]").replace("<", "[lt]").replace(">", "[gt]")
         writer.write(data)
 
-def writenml(fing, writer, indent="", addindent="", newl=""):
-    if isinstance(fing, _d.Element):
+def writenml(node, writer, indent="", addindent="", newl=""):
+    if isinstance(node, _d.Element):
         # indent = current indentation
         # addindent = indentation to add to higher levels
         # newl = newline string
-        if "|" in fing.tagName:
-            raise ValueError("pipe in tag name %r" % fing.tagName)
-        writer.write(indent+"<" + fing.tagName)
+        if "|" in node.tagName:
+            raise ValueError("pipe in tag name %r" % node.tagName)
+        writer.write(indent+"<" + node.tagName)
 
-        attrs = fing._get_attributes()
+        attrs = node._get_attributes()
         a_names = attrs.keys()
         a_names.sort()
 
@@ -36,37 +36,37 @@ def writenml(fing, writer, indent="", addindent="", newl=""):
             writer.write("<%s|" % a_name)
             _write_data(writer, attrs[a_name].value)
             writer.write(">")
-        if fing.childNodes:
+        if node.childNodes:
             writer.write("|")
-            if (len(fing.childNodes) == 1 and
-                    fing.childNodes[0].nodeType == _d.Node.TEXT_NODE):
-                writenml(fing.childNodes[0], writer, '', '', '')
+            if (len(node.childNodes) == 1 and
+                    node.childNodes[0].nodeType == _d.Node.TEXT_NODE):
+                writenml(node.childNodes[0], writer, '', '', '')
             else:
                 writer.write(newl)
-                for cnode in fing.childNodes:
+                for cnode in node.childNodes:
                     writenml(cnode, writer, indent+addindent, addindent, newl)
                 writer.write(indent)
             writer.write(">%s"%(newl))
         else:
             writer.write(">%s"%(newl))
-    elif isinstance(fing, _d.Text): #including CDATASection
-        _write_data(writer, "%s%s%s" % (indent, fing.data, newl))
+    elif isinstance(node, _d.Text): #including CDATASection
+        _write_data(writer, "%s%s%s" % (indent, node.data, newl))
     # Unclear what the following are supposed to be rendered as:
-    elif isinstance(fing, _d.DocumentType):
+    elif isinstance(node, _d.DocumentType):
         pass
-    elif isinstance(fing, _d.ProcessingInstruction):
-        writer.write("%s<?%s|%s>%s" % (indent, fing.target, fing.data, newl))
-    elif isinstance(fing, _d.Comment):
+    elif isinstance(node, _d.ProcessingInstruction):
+        writer.write("%s<?%s|%s>%s" % (indent, node.target, node.data, newl))
+    elif isinstance(node, _d.Comment):
         writer.write("%s<!--|" % indent)
-        _write_data(writer, fing.data)
+        _write_data(writer, node.data)
         writer.write(">%s" % newl)
-    elif isinstance(fing, _d.Document):
-        for cnode in fing.childNodes:
+    elif isinstance(node, _d.Document):
+        for cnode in node.childNodes:
             writenml(cnode, writer, indent, addindent, newl)
     else:
         raise TypeError
 
-def tonml(fing, encoding="utf-8", indent="", newl=""):
+def tonml(node, encoding="utf-8", indent="", newl=""):
     # indent = the indentation string to prepend, per level
     # newl = the newline string to append
     writer = _d._get_StringIO()
@@ -74,5 +74,5 @@ def tonml(fing, encoding="utf-8", indent="", newl=""):
         import codecs
         # Can't use codecs.getwriter to preserve 2.0 compatibility
         writer = codecs.lookup("utf-8")[3](writer)
-    writenml(fing, writer, "", indent, newl)
+    writenml(node, writer, "", indent, newl)
     return writer.getvalue()

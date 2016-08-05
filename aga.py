c0a398b0270f3d6a5ff2c7773aab2927baa7d671
Thomas Hori <thomas.hori@liddicott.com>
2016-08-05 13:46:00 +0100

    6.1.2 - fix errors introduced into romkan, bring into conformance with jawikipedia page on JIS X 4063:2000.

--- aga.py
+++ aga.py
@@ -1,1 +1,11 @@
 from mdplay.romkan import *
+
+b=open("standard.py").read()
+b=eval(b)
+for i,j in b:
+    for k in j:
+        if to_hiragana(k)!=i:
+            print "Romaji:",k
+            print "Standard says:",i
+            print "Library says:",to_hiragana(k)
+            print

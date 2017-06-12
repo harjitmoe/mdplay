512c310f27efbf4ba3843e88795a3afad6c02be3
HarJIT <harjit@harjit.moe>
2017-06-12 10:48:18 +0100

    Document LinestackIter.

--- mdplay/LinestackIter.py
+++ mdplay/LinestackIter.py
@@ -5,41 +5,55 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
 try:
-    StopIteration=StopIteration
+    StopIteration = StopIteration
 except NameError:
-    StopIteration=IndexError
+    StopIteration = IndexError
+
 class LinestackIter(object):
-    _stack=None
-    _n=None
-    _c=None
-    def __init__(self,stack):
-        self._stack=list(stack)
-        self._n=-1
-        self._c=0 #Number of calls to __next__
+    """Specialised iterator for a list of lines, allowing peeking as well as
+    re-running of previously seen lines.
+    """
+    
+    _stack = None
+    _n = None
+    _c = None
+    
+    def __init__(self, stack):
+        self._stack = list(stack)
+        self._n = -1
+        self._c = 0 #Number of calls to __next__
+    
     def __next__(self):
-        self._c+=1
-        self._n+=1
-        if self._n==len(self._stack):
+        self._c += 1
+        self._n += 1
+        if self._n == len(self._stack):
             return ""
-        if self._n>=(len(self._stack)+1):
+        if self._n >= (len(self._stack) + 1):
             raise StopIteration
         return self._stack[self._n]
-    def peek_back(self,n=1):
-        if (self._n-n)<0:
+    
+    def peek_back(self, n=1):
+        """Return the previous line, or None if there isn't one."""
+        if (self._n - n) < 0:
             return None
-        return self._stack[self._n-n]
-    def peek_ahead(self,n=1):
-        if (self._n+n)>=len(self._stack):
+        return self._stack[self._n - n]
+    
+    def peek_ahead(self, n=1):
+        """Return the next line, or None if there isn't one."""
+        if (self._n + n) >= len(self._stack):
             return None
-        return self._stack[self._n+n]
+        return self._stack[self._n + n]
+    
     def rtpma(self): #Run that past me again
-        self._n-=1
-    #
-    # Tell Python to use iterator __next__/next API
+        """Make the next iteration yield the current line again."""
+        self._n -= 1
+    
+    # Is itself an iterator, so its iterator is itself.
     def __iter__(self):
         return self
-    #
+    
     # Wrappers for older iterator APIs
+    # In particular, next() is needed on Python 2.
     def next(self):
         return self.__next__()
     def __getitem__(self,i):
@@ -47,3 +61,4 @@ class LinestackIter(object):
             return self.__next__()
         else:
             raise TypeError("indexing an iterator")
+

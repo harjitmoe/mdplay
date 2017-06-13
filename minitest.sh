a01e93a673fad1f358d3f515912c6df172a1c8d4
HarJIT <harjit@harjit.moe>
2017-06-13 23:18:58 +0100

    Mostly works except for the roundtripping.

--- minitest.sh
+++ minitest.sh
@@ -1,2 +1,4 @@
-python pymdplay.py -o out.html -f html test.md
-python pymdplay.py -o out.repr -f debug test.md
+python -3 pymdplay.py -o out.html -f html test.md
+python -3 pymdplay.py -o out.repr -f debug test.md
+python -3 pymdplay.py -o roundtrip.md -f md test.md
+python -3 pymdplay.py -o roundtrip2.md -f md roundtrip.md
